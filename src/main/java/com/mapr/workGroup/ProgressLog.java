9d95bb35cd28106982229837ca8d0eebc1b69fce
tdunning <tdunning@apache.org>
2013-03-13 17:25:51 -0700

    Simple example of protobuf based logging.

---
+++ src/main/java/com/mapr/workGroup/ProgressLog.java
@@ -0,0 +1,114 @@
+package com.mapr.workGroup;
+
+import com.google.common.collect.Maps;
+
+import java.io.*;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Record progress.
+ */
+public class ProgressLog {
+    private FileOutputStream stream;
+    private final long id;
+
+    private ProgressLog(String name, FileOutputStream stream) throws IOException {
+        this.stream = stream;
+        this.id = new Random().nextLong();
+        ProgressNote.Update.Builder out = ProgressNote.Update.newBuilder();
+        out.getStartBuilder()
+                .setId(id)
+                .setTime(System.currentTimeMillis())
+                .setName(name)
+                .build();
+        out.build().writeDelimitedTo(stream);
+    }
+
+    public static ProgressLog open(String ourName, File f) throws IOException {
+        return new ProgressLog(ourName, new FileOutputStream(f, true));
+    }
+
+    public void increment(String name, int delta) throws IOException {
+        ProgressNote.Update.Builder out = ProgressNote.Update.newBuilder();
+        ProgressNote.Progress.Builder note = out.getProgressBuilder()
+                .setId(id)
+                .setTime(System.currentTimeMillis());
+        note.addCountBuilder().setKey(name).setIntegerIncrement(delta).build();
+        out.build().writeDelimitedTo(stream);
+    }
+
+    public void note(String note) throws IOException {
+        ProgressNote.Update.Builder out = ProgressNote.Update.newBuilder();
+        out.getNoteBuilder()
+                .setId(id)
+                .setTime(System.currentTimeMillis())
+                .setNote(note)
+                .build();
+        out.build().writeDelimitedTo(stream);
+    }
+
+    public void finish() throws IOException {
+        ProgressNote.Update.Builder out = ProgressNote.Update.newBuilder();
+        out.getCompleteBuilder()
+                .setId(id)
+                .setTime(System.currentTimeMillis())
+                .setExitStatus(0)
+                .build();
+        out.build().writeDelimitedTo(stream);
+        stream.close();
+        stream = null;
+    }
+
+    public void fail(String message, Throwable e) throws IOException {
+        StringWriter stack = new StringWriter();
+        PrintWriter pw = new PrintWriter(stack);
+        e.printStackTrace(pw);
+        pw.close();
+
+        ProgressNote.Update.Builder out = ProgressNote.Update.newBuilder();
+        out.getCompleteBuilder()
+                .setId(id)
+                .setTime(System.currentTimeMillis())
+                .setExitMessage(message)
+                .setStackTrace(stack.toString())
+                .setExitStatus(1)
+                .build();
+        out.build().writeDelimitedTo(stream);
+
+        stream.close();
+        stream = null;
+    }
+
+    public static Map<String, Double> readTotals(File log) throws IOException {
+        FileInputStream input = new FileInputStream(log);
+
+        Map<String, Double> r = Maps.newHashMap();
+        ProgressNote.Update update = ProgressNote.Update.parseDelimitedFrom(input);
+        while (update != null) {
+            if (update.hasProgress()) {
+                for (ProgressNote.KeyValue kv : update.getProgress().getCountList()) {
+                    Double v = r.get(kv.getKey());
+                    if (v == null) {
+                        v = 0.0;
+                        r.put(kv.getKey(), v);
+                    }
+                    if (kv.hasDvalue()) {
+                        v = kv.getDvalue();
+                    } else if (kv.hasDoubleIncrement()) {
+                        v += kv.getDoubleIncrement();
+                    } else if (kv.hasIvalue()) {
+                        v = (double) kv.getIvalue();
+                    } else if (kv.hasIntegerIncrement()) {
+                        v += kv.getIntegerIncrement();
+                    } else {
+                        // missing value = 0
+                    }
+                    r.put(kv.getKey(), v);
+                }
+            }
+            update = ProgressNote.Update.parseDelimitedFrom(input);
+        }
+        return r;
+    }
+}
---
+++ src/test/java/com/mapr/workGroup/ProgressLogTest.java
@@ -0,0 +1,45 @@
+package com.mapr.workGroup;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProgressLogTest {
+    @Test
+    public void testRoundTrip() throws IOException {
+        File logFile = File.createTempFile("foo", "log");
+        ProgressLog log = ProgressLog.open("test", logFile);
+
+        log.note("phase 1");
+        log.increment("k", 3);
+        log.increment("x", 1);
+        log.increment("k", 2);
+        log.note("phase 2");
+        log.increment("x", 10);
+
+        log.finish();
+
+        Map<String, Double> r = ProgressLog.readTotals(logFile);
+        assertEquals(5.0, r.get("k"), 0.0);
+        assertEquals(11.0, r.get("x"), 0.0);
+    }
+
+    @Test
+    public void testBackTrace() throws IOException {
+        File logFile = File.createTempFile("foo", "log");
+        ProgressLog log = ProgressLog.open("test", logFile);
+
+        log.note("phase 1");
+        log.increment("k", 3);
+        log.increment("x", 1);
+        log.fail("failed", new Exception());
+
+        Map<String, Double> r = ProgressLog.readTotals(logFile);
+        assertEquals(5.0, r.get("k"), 0.0);
+        assertEquals(11.0, r.get("x"), 0.0);
+    }
+}
